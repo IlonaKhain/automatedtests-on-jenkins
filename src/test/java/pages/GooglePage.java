f2a90c93d9dabfeca64afa1b1f7eed19646c3efd
IlonaKhain <hain@dlit.dp.ua>
2020-10-06 23:10:20 +0300

    Add files via upload
    to initial directory

---
+++ src/test/java/pages/GooglePage.java
@@ -0,0 +1,11 @@
+package pages;
+
+import org.openqa.selenium.By;
+
+import static com.codeborne.selenide.Selenide.$;
+
+public class GooglePage {
+    public void searchFor(String text) {
+        $(By.name("q")).val(text).pressEnter();
+    }
+}
